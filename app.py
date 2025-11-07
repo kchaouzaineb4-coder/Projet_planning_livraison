e2909f24374e77483b4a35f0014692c7cea4ec6f
Zaineb KCHAOU <kchaouzaineb4@gmail.com>
2025-11-07 16:38:42 +0100

    Correction : support fichiers .xls et mise à jour requirements

--- app.py
+++ app.py
@@ -344,8 +344,8 @@ with tab_charts:
     col3, col4 = st.columns(2)
     with col3:
         st.plotly_chart(
-            px.bar(st.session_state.df_city, x="Ville", y="Nombre livraisons",
-                   title="Nombre de livraisons par ville"),
+            px.bar(st.session_state.df_city, x="Ville", y="Nombre de BLs",
+                   title="Nombre de BLs par ville"),
             use_container_width=True
         )
     with col4:
