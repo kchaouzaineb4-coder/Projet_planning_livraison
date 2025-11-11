e6f81758d649215917a962977688e7f133a100e1
Zaineb KCHAOU <kchaouzaineb4@gmail.com>
2025-11-11 11:21:43 +0100

    Correction : support fichiers .xls et mise à jour requirements

--- app.py
+++ app.py
@@ -317,7 +317,7 @@ with tab_grouped:
     
     /* En-têtes du tableau - BLEU ROYAL SANS DÉGRADÉ */
     .custom-table th {
-        background-color: #3730A3;
+        background-color: #1560BD;
         color: white;
         padding: 12px 8px;
         text-align: center;
