53199ae704329ad3c4a58a75e9979421cda1399f
Zaineb KCHAOU <kchaouzaineb4@gmail.com>
2025-11-11 11:12:54 +0100

    Correction : support fichiers .xls et mise à jour requirements

--- app.py
+++ app.py
@@ -317,7 +317,7 @@ with tab_grouped:
     
     /* En-têtes du tableau - BLEU ARCTIQUE SANS DÉGRADÉ */
     .custom-table th {
-        background-color: #87CEEB;
+        background-color: #ECFEFF;
         color: white;
         padding: 12px 8px;
         text-align: center;
