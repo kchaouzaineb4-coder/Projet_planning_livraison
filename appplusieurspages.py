7f2920be0aba8c39d9b0a1591e5b36ba4b888079
Zaineb KCHAOU <kchaouzaineb4@gmail.com>
2025-12-10 10:02:45 +0100

    Correction : support fichiers .xls et mise à jour requirements

--- appplusieurspages.py
+++ appplusieurspages.py
@@ -218,7 +218,7 @@ def page_import():
     st.markdown("<h1 class='main-header'>1. 📥 Importation des données</h1>", unsafe_allow_html=True)
     
     st.markdown("""
-    <div style='background: linear-gradient(135deg, #1E3A8A 0%, #3B82F6 100%);
+    <div style='background: linear-gradient(135deg, #0369A1 0%, #3B82F6 100%);
                 padding: 2rem;
                 border-radius: 10px;
                 color: white;
